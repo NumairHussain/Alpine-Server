2ba7976793063c6ae77930d1f162a19e6aea7bf8
NumairHussain <numeyg@gmail.com>
2026-04-13 14:32:16 -0500

    attempted to fix deploy script to do all services

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,35 +11,77 @@ STACKS[Nginx]="./nginx/nginx/conf.d"
 STACKS[Olympus]="./olympus"
 STACKS[Actual-Budget]="./actual-budget"
 
+working_directory="$(dirname "$0")"
+all_services_flag=false
+
 # Check if stack exists
 if [[ ! -v "STACKS[$STACK]" ]]; then
     echo "Error: Stack '$STACK' not found"
     exit 1
 fi
 
-cd "${STACKS[$STACK]}"
-if [[ $? -ne 0 ]]; then
-    echo "Error: Failed to navigate to stack directory"
-    exit 1
-fi
+if [[ "$STACK" == "All Services" ]]; then
+    all_services_flag=true
+    echo "Performing '$ACTION' on all services..."
+else
+    echo "Performing '$ACTION' on $STACK..."
+    cd "${STACKS[$STACK]}"
+    if [[ $? -ne 0 ]]; then
+        echo "Error: Failed to navigate to stack directory"
+        exit 1
+    fi
+fi 
+
 
 # Execute action
 case $ACTION in
     "Start")
-        echo "Starting $STACK..."
-        docker-compose up -d
+        if [[ "$all_services_flag" == true ]]; then
+            for key in "${!STACKS[@]}"; do
+                dir="${STACKS[$key]}"
+                echo "Starting $key (dir=$dir)..."
+                (cd $dir && docker compose up -d)
+            done
+        else
+            echo "Starting $STACK..."
+            docker compose up -d
+        fi
         ;;
     "Stop")
-        echo "Stopping $STACK..."
-        docker-compose down
+        if [[ "$all_services_flag" == true ]]; then
+            for key in "${!STACKS[@]}"; do
+                dir="${STACKS[$key]}"
+                echo "Stopping $key (dir=$dir)..."
+                (cd $dir && docker compose down)
+            done
+        else
+            echo "Stopping $STACK..."
+            docker compose down
+        fi
         ;;
     "Restart")
-        echo "Restarting $STACK..."
-        docker-compose restart
+        if [[ "$all_services_flag" == true ]]; then
+            for key in "${!STACKS[@]}"; do
+                dir="${STACKS[$key]}"
+                echo "Stopping $key (dir=$dir)..."
+                (cd $dir && docker compose restart)
+            done
+        else
+            echo "Restarting $STACK..."
+            docker compose restart
+        fi
         ;;
     "Update")
-        echo "Updating $STACK..."
-        docker-compose pull && docker-compose up -d
+        if [[ "$all_services_flag" == true ]]; then
+            for key in "${!STACKS[@]}"; do
+                dir="${STACKS[$key]}"
+                echo "Stopping $key (dir=$dir)..."
+                (cd $dir && docker compose pull && docker compose up -d)
+            done
+        else
+            echo "Updating $STACK..."
+            docker compose pull && docker compose up -d
+        fi
         ;;
     *)
         echo "Unknown action: $ACTION"
